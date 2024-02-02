3a61f6e7a868db0e42abdb0249f4f94918db5a5e
Alkis Gotovos <alkis.gotovos@gmail.com>
2024-02-02 17:38:58 +0100

    Functional app

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,148 @@
-import Image from "next/image";
+'use client'
+
+import { twMerge } from 'tailwind-merge'
+import Button from '@/app/Button'
+import { useState } from 'react'
+
+type Slip = {
+  slip_id: number
+  advice: string
+}
+
+const defaultAdvice = {
+  slip_id: 117,
+  advice:
+    "It is easy to sit up and take notice, what's difficult is getting up and taking action",
+}
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [advice, setAdvice] = useState(defaultAdvice)
+
+  async function onClick() {
+    const minDelaySeconds = 2
+    setIsLoading(true)
+    const start = Date.now()
+    const result = await getAdvice()
+    const end = Date.now()
+    const delay = minDelaySeconds * 1000 - (end - start)
+    if (delay > 0) {
+      await new Promise((resolve) => setTimeout(resolve, delay))
+    }
+    setAdvice(result)
+    setIsLoading(false)
+  }
+
+  async function getAdvice(): Promise<Slip> {
+    const response = await fetch('https://api.adviceslip.com/advice', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = (await response.json()) as {
+      slip: { id: number; advice: string }
+    }
+    return { slip_id: data.slip.id, advice: data.slip.advice }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div
+      className={`flex flex-col min-h-dvh min-w-fit bg-background tablet:items-center tablet:justify-center`}
+    >
+      <div className={`flex flex-col px-4 pt-[120px] pb-[200px] tablet:p-0`}>
+        <div
+          className={twMerge(
+            `relative`,
+            `flex flex-col bg-dark_grayish_blue items-center rounded-[10px] px-6 pt-10 pb-16`,
+            `tablet:w-[540px] tablet:p-12 tablet:pb-[72px] tablet:rounded-[15px]`
+          )}
+        >
+          <Title className={`mb-6`} advice={advice} isLoading={isLoading} />
+          <Advice
+            className={`mb-6 tablet:mb-10`}
+            advice={advice}
+            isLoading={isLoading}
+          />
+          <Divider />
+          <Button
+            className={`absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2`}
+            onClick={onClick}
+            isLoading={isLoading}
+          />
         </div>
       </div>
+    </div>
+  )
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+function Title({
+  className = '',
+  advice,
+  isLoading = false,
+}: {
+  className?: string
+  advice: Slip
+  isLoading?: boolean
+}) {
+  return (
+    <h1
+      className={twMerge(
+        `text-[11px] text-primary font-semibold leading-[120%] uppercase tracking-[3.457px]`,
+        `tablet:text-[13px] tablet:tracking-[4.086px]`,
+        !isLoading && `transition-[color,text-opacity] duration-500`,
+        isLoading &&
+          'text-primary/0 bg-primary bg-opacity-20 animate-pulse rounded-3xl',
+        className
+      )}
+    >
+      Advice #{advice.slip_id}
+    </h1>
+  )
+}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+function Advice({
+  className = '',
+  advice,
+  isLoading = false,
+}: {
+  className?: string
+  advice: Slip
+  isLoading?: boolean
+}) {
+  return (
+    <p
+      className={twMerge(
+        `text-center text-[24px] text-lightcyan font-bold leading-[140%] tracking-[-0.257px]`,
+        `tablet:text-[28px] tablet:tracking-[-0.3px]`,
+        !isLoading && `transition-[color] duration-500`,
+        isLoading &&
+          'text-lightcyan/0 bg-lightcyan bg-opacity-20 animate-pulse rounded-3xl',
+        className
+      )}
+    >
+      “{advice.advice}”
+    </p>
+  )
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+function Divider({ className = '' }: { className?: string }) {
+  return (
+    <div
+      className={twMerge(`w-full flex flex-row gap-4 items-center`, className)}
+    >
+      <div className={`grow bg-grayish_blue h-px`} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="16"
+        viewBox="0 0 20 16"
+        fill="none"
+      >
+        <rect width="6" height="16" rx="3" fill="#CEE3E9" />
+        <rect x="14" width="6" height="16" rx="3" fill="#CEE3E9" />
+      </svg>
+      <div className={`grow bg-grayish_blue h-px`} />
+    </div>
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      background: '#202733',
+      dark_grayish_blue: '#313A48',
+      grayish_blue: '#4F5D74',
+      primary: '#53FFAA',
+      lightcyan: '#CEE3E9',
+    },
+    screens: {
+      tablet: '600px',
+    },
+    boxShadow: {
+      button_hover: '0px 0px 40px 0px #53FFAA',
     },
   },
   plugins: [],
-};
-export default config;
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+}
+export default config
